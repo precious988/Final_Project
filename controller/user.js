b2248c63511feb6e42d7fe067b6649c28212fc77
Okeyzie <leookeyzie@gmail.com>
2025-09-15 17:10:17 +0100

    adding get one

--- controller/user.js
+++ controller/user.js
@@ -36,3 +36,23 @@ exports.register = async (req, res) => {
     })
   }
 }
+exports.getAuser = async (req, res) => {
+    try {
+        const userId = req.params.id;
+        const user = await userModel.findById(userId);
+        if (!user) {
+            return res.status(404).json({
+                message: 'User not found'
+            });
+        }
+        res.status(200).json({
+            message: 'User retrieved successfully',
+            data: user
+        });
+    } catch (error) {
+        res.status(500).json({
+            status: 'internal server error' + error.message,
+            message: error.message
+        });
+    }
+}
